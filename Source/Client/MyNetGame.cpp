b82ca518c3b843fbc64a2f31b0d8d6ae0e2068ab
AlexAllmanLaptop <xalexxd@hotmail.co.uk>
2018-03-23 14:09:35 +0000

    Base enemy classes made with the ability for easy expansion

--- Source/Client/MyNetGame.cpp
+++ Source/Client/MyNetGame.cpp
@@ -49,6 +49,8 @@ bool MyNetGame::init()
 	th = std::thread(&ClientComponent::consumeEvents, &network);
 	th.detach();
 
+
+	////Loading in units and initialising one, Change the number index in unit types to determine which unit is loaded from the json file
 	UnitType::load();
 	gunner_enemy.reset(UnitType::unit_types[0].createUnit(renderer.get()));
 
