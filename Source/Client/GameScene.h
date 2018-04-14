b59bde46ac20bfd251500899b3179821e7817737
Unknown <xalexxd@hotmail.co.uk>
2018-04-14 15:44:47 +0100

    Reconnect logic is in place

--- Source/Client/GameScene.h
+++ Source/Client/GameScene.h
@@ -88,6 +88,7 @@ private:
 	void unitsUpdate(const ASGE::GameTime& ms);
 	void chatUpdate(const ASGE::GameTime& ms);
 	void unitNetworkUpdate(const ASGE::GameTime& ms);
+	void updateReconnectee(const ASGE::GameTime& ms);
 
 	int whichTurn();
 	int whichPlayer();
